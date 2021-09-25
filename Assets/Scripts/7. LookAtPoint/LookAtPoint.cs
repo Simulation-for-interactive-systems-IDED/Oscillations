4a3a75a9ba7e3ab4cc72636997ba81bb8f35cfc4
Romualdo Villalobos <romualdo.villalobos@wizard.com.co>
2021-09-25 17:14:25 -0500

    Ready oscillations first part

--- Assets/Scripts/7. LookAtPoint/LookAtPoint.cs
+++ Assets/Scripts/7. LookAtPoint/LookAtPoint.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtMousePosition : MonoBehaviour
+public class LookAtPoint : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
         Vector4 worldMousePosition = GetWorldMousePosition();
-        float radians = Mathf.Atan(worldMousePosition.y / worldMousePosition.x) - Mathf.PI / 2;
+        LookAt(worldMousePosition);
+    }
+
+    private void LookAt(Vector2 targetPosition)
+    {
+        Vector2 thisPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 forward = targetPosition - thisPosition;
+        float radians = Mathf.Atan2(forward.y, forward.x) - Mathf.PI / 2;
         RotateZ(radians);
     }
 
